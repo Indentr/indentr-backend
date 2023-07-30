4090896bed321332d4b3c5c3983e47e4a9e86706
joethompson <thompsonjoejj@gmail.com>
2023-07-30 23:47:28 +0100

    small refactor

--- app/routes/user.py
+++ app/routes/user.py
@@ -8,12 +8,6 @@ from app.services.user import check_user
 router = APIRouter(prefix="/login", tags=["Login"])
 
 
-# @router.get("/login-status")
-# def get_user(auth=Depends(JWTBearer())):
-#     return {
-#         "status": True,
-#     }
-
 
 @router.post("/")
 def post_user_login(body: UserLoginRequest, request: Request):
