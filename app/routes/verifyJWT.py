453e1bc32f8752cd312f0db688b3d28762d215c9
joethompson <thompsonjoejj@gmail.com>
2023-08-02 00:12:35 +0100

    profile get endpoint working

--- app/routes/verifyJWT.py
+++ app/routes/verifyJWT.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from app.middleware.jwt import JWTBearer, decodeJWT
+from app.middleware.jwt import JWTBearer
 
 router = APIRouter(prefix="/verifyJWT", tags=["verifyJWT"])
 
@@ -12,13 +12,13 @@ def verifyJWT(access_token=Depends(JWTBearer())):
 
     This route is protected by the `JWTBearer` dependency, which checks the validity of the access token.
     If the access token is valid, the `decodeJWT` function is used to decode and verify the token.
+    If the access token is not valid, the endpoint will return a 403 error message.
 
     Parameters:
     - access_token (str, optional): The access token to be verified. This token is obtained from the `Authorization` header with the "Bearer" scheme.
 
     Returns:
-    dict: A dictionary containing the decoded access token information.
+    None
     """
 
-    print("access_token: ", decodeJWT(access_token))
-    return {}
+    return None
