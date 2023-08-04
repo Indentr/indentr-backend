055da6658c8cc7444a9e56989abc4980485c08d0
joethompson <thompsonjoejj@gmail.com>
2023-08-04 23:55:43 +0100

    added saveDetails endpoint to save updated details to mongodb, also updated jwt to use _id instead of email

--- app/routes/profile.py
+++ app/routes/profile.py
@@ -1,8 +1,10 @@
 import base64
 
+from bson import ObjectId
 from fastapi import APIRouter, Depends, HTTPException, Request
 
 from app.middleware.jwt import JWTBearer, decodeJWT
+from app.models.user import userDetails
 
 router = APIRouter(prefix="/profile", tags=["Profile"])
 
@@ -26,13 +28,9 @@ def get_profile(request: Request, access_token=Depends(JWTBearer())):
     db = request.app.state.db
     token = decodeJWT(access_token)
 
-    user_email = token['user_id']
-
-    letters_collection = db['letters']
-    letters = list(letters_collection.find({'user_email': user_email}).sort('_id', -1).limit(3))
-
+    user_id = token['user_id']
     users_collection = db['users']
-    user = users_collection.find_one({'email': user_email}, {'_id': 0, 'email': 1, 'phone': 1, 'address': 1, 'img': 1})
+    user = users_collection.find_one({'_id': ObjectId(user_id)}, {'_id': 0, 'email': 1, 'phone': 1, 'address': 1, 'img': 1})
     if user is not None:
         user_object = {
             'email': user['email'],
@@ -40,6 +38,10 @@ def get_profile(request: Request, access_token=Depends(JWTBearer())):
             'address': user.get('address', None),
             'img': base64.b64encode(user.get('img', b'')).decode('utf-8')
         }
+
+    letters_collection = db['letters']
+    letters = list(letters_collection.find({'user_email': user['email']}).sort('_id', -1).limit(3))
+
 
     # Access the 'createdAt' field for each letter and convert it to time date format
     # Add a createdAt attribute to letter
@@ -102,6 +104,56 @@ async def saveImg(request: Request, access_token=Depends(JWTBearer())):
         raise HTTPException(status_code=500, detail="Failed to save image") from e
 
 
+
+
+
+
+@router.post("/saveDetails")
+async def saveDetails(body: userDetails, request: Request, access_token=Depends(JWTBearer())):
+    """
+    Save User Details
+
+    Save the user's email, phone number, and address to the database.
+
+    Args:
+        body (userDetails): The user details to be updated.
+        request (Request): The request object from FastAPI.
+        access_token (str, optional): The JWT access token obtained from the `JWTBearer` dependency.
+
+    Returns:
+        dict: A dictionary containing the updated user details.
+
+    Raises:
+        HTTPException: If there are any errors during the update process.
+    """
+
+    try:
+        email = body.email
+        phone = body.phone
+        address = body.address
+
+        if not email or not phone or not address:
+            raise HTTPException(status_code=400, detail="No data provided")
+
+        # Extract user's email from JWT token
+        token = decodeJWT(access_token)
+        user_email = token['user_id']
+
+
+        # Update the user's image in MongoDB
+        dbUsers = request.app.state.db.users
+        dbUsers.update_one(
+            {'email': user_email},
+            {'$set': {
+                'email': email,
+                'phone': phone,
+                'address': address
+            }}
+        )
+
+        return { 'email': email, 'phone': phone, 'address': address }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Failed to save details") from e
 
 
 
