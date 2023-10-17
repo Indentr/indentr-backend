1e0292e03c4dbbe6eb2229ebab162c08e6adc8ff
joethompson <thompsonjoejj@gmail.com>
2023-10-17 16:19:12 +0100

    added register endpoint and removed unnecessary function from login endpoint

--- app/models/login.py
+++ app/models/login.py
@@ -4,3 +4,9 @@ from pydantic import BaseModel
 class UserLoginRequest(BaseModel):
     email: str
     password: str
+
+
+class UserRegisterRequest(BaseModel):
+    name: str 
+    email: str 
+    password: str 
