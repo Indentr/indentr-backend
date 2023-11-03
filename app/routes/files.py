34ac3bba656755ba769c4a676204f50651b174f4
joethompson <thompsonjoejj@gmail.com>
2023-11-03 17:31:32 +0000

    fix: removed unformatted line

--- app/routes/files.py
+++ app/routes/files.py
@@ -32,8 +32,6 @@ def get_files(request: Request, access_token=Depends(JWTBearer())):
     # Find and retrieve the letters with projection and sorting
     letters = list(letters_collection.find({"user_id": user_id}, projection=projection).sort(sort_field))
 
-        print("hello")
-
     # Access the 'createdAt' field for each letter and convert it to time date format
     # Add a createdAt attribute to letter
     for letter in letters:
