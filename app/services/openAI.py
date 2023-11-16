07ed780e44853e76cb697ad799090a8687eef689
joethompson <thompsonjoejj@gmail.com>
2023-11-16 20:01:18 +0000

    upgrade to GPT 4 and including pricing list in prompt

--- app/services/openAI.py
+++ app/services/openAI.py
@@ -17,7 +17,7 @@ async def ask_gpt(prompt: str, system_prompt: str):
     openai.api_key = OPENAI_API_KEY
     try:
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=[{"role": "system", "content": system_prompt}, {"role": "user", "content": prompt}], temperature=0.0
+            model="gpt-4", messages=[{"role": "system", "content": system_prompt}, {"role": "user", "content": prompt}], temperature=0.0
         )
 
         content = response["choices"][0]["message"]["content"]
