3ac1fa37d8a54711478e533e44d8d158b49613d5
joethompson <thompsonjoejj@gmail.com>
2023-12-19 13:31:30 +0000

    Remove .DS_Store files

--- app/database/schemas/triage.py
+++ app/database/schemas/triage.py
@@ -1,21 +1,21 @@
-from mongoengine import Document, EmbeddedDocument, fields
+from mongoengine import (
+    Document, 
+    EmbeddedDocument,
+    ReferenceField,
+    fields
+)
 
 from app.database.schemas.practice import Practice
+from app.database.schemas.user import User
 
 
-class TriageResponse(EmbeddedDocument):
+class Triage(Document):
+    practice_id = ReferenceField(Practice, required=True)
+    user_id = ReferenceField(User, required=True)
     diagnosis = fields.StringField()
     general_overview = fields.StringField()
     severity = fields.IntField()
     time_urgency = fields.StringField()
     patient_instructions = fields.StringField()
 
-
-class Triage(Document):
-    forename = fields.StringField()
-    surname = fields.StringField()
-    dob = fields.StringField()
-    email = fields.EmailField()
-    phone_number = fields.StringField()
-    practice_id = fields.ReferenceField(Practice)
-    triage_response = fields.EmbeddedDocumentField(TriageResponse)
+    meta = {"collection": "triage_responses"}  # Specify the collection name
