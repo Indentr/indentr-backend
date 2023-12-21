3f8d676ab96e98bf89fc7373c324a3443e4b224c
joethompson <thompsonjoejj@gmail.com>
2023-12-21 16:36:32 +0000

    Added opened field to Triage document

--- app/database/schemas/triage.py
+++ app/database/schemas/triage.py
@@ -1,14 +1,23 @@
-from mongoengine import Document, ReferenceField, fields
+from mongoengine import (
+    Document, 
+    EmbeddedDocument,
+    ReferenceField,
+    StringField,
+    IntField,
+    BooleanField
+)
 
-from app.database.schemas.patient import Patient
 from app.database.schemas.practice import Practice
+from app.database.schemas.patient import Patient
+from app.database.schemas.user import User
 
 
 class Triage(Document):
     practice_id = ReferenceField(Practice, required=True)
     patient_id = ReferenceField(Patient)
-    diagnosis = fields.StringField()
-    general_overview = fields.StringField()
-    severity = fields.IntField()
+    diagnosis = StringField()
+    general_overview = StringField()
+    severity = IntField()
+    opened = BooleanField(default=False)
 
     meta = {"collection": "triage_responses"}
