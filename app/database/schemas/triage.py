d6184f78bdc749641f6f58a117bb08cd5fbe79de
joethompson <thompsonjoejj@gmail.com>
2023-12-30 22:37:19 +0000

    Added extra fields to triage request document - created at field - requested date - GPT_QA (the q's and a's from gpt/patient)

--- app/database/schemas/triage.py
+++ app/database/schemas/triage.py
@@ -1,15 +1,18 @@
-from mongoengine import BooleanField, Document, IntField, ReferenceField, StringField
-
+from mongoengine import BooleanField, Document, IntField, ReferenceField, StringField, DateTimeField
+from datetime import datetime
 from app.database.schemas.patient import Patient
 from app.database.schemas.practice import Practice
 
 
 class Triage(Document):
     practice_id = ReferenceField(Practice, required=True)
-    patient_id = ReferenceField(Patient)
+    patient_id = ReferenceField(Patient, required=True)
     diagnosis = StringField()
     general_overview = StringField()
     severity = IntField()
     opened = BooleanField(default=False)
+    created_at = DateTimeField(default=datetime.utcnow)
+    requested_date = DateTimeField()
+    GPT_QA = StringField()
 
     meta = {"collection": "triage_responses"}
