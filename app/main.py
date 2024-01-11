845508124c4e8f4607baa0bee841c6163289cff1
joethompson <thompsonjoejj@gmail.com>
2024-01-11 13:28:33 +0000

    Fixed pytest warning after upgrading fastapi package

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from starlette_context.middleware import RawContextMiddleware
 
 from app.constants import ALLOWED_ORIGINS, DB_URI
 from app.database.db import connect_to_mongoengine
-from app.routes import auth, create, files, profile, triage
+from app.routes import auth, create, files, profile, triage, vector_letter
 
 middleware = [Middleware(RawContextMiddleware)]
 
@@ -37,7 +37,6 @@ app.add_middleware(
 )
 
 
-@app.on_event("startup")
 async def startup_event():
     # When server starts up establish connection with mongodb
     connect_to_mongoengine(DB_URI)
@@ -48,8 +47,11 @@ def get_health():
     return {"message": "Welcome to Indentr!"}
 
 
+app.add_event_handler("startup", startup_event)
+
 app.include_router(auth.router)
 app.include_router(profile.router)
 app.include_router(files.router)
 app.include_router(create.router)
 app.include_router(triage.router)
+app.include_router(vector_letter.router)
