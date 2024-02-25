02b8e9a8d9afc0e6f9fe3c95310f78942d3831a7
Robert Lee <robbielee543@gmail.com>
2024-02-25 22:02:18 +0000

    black, ruff

--- app/routes/files.py
+++ app/routes/files.py
@@ -21,11 +21,11 @@ from app.database.crud import (
     retrieve_letters_alphabet_status,
     retrieve_note,
     retrieve_notes_alphabet_status,
+    retrieve_patient_by_email,
     retrieve_patient_by_id,
     retrieve_patients_alphabet_status,
     retrieve_patients_last_three_letters,
     retrieve_patients_last_three_notes,
-    retrieve_patient_by_email,
     retrieve_user_letter,
     update_letter,
     update_note,
@@ -83,13 +83,13 @@ def get_file(file_type: str, file_id: str, access_token=Depends(JWTBearer())):
         practice_id = token["practice_id"]
         letters = []
         notes = []
-        patient_details=[]
+        patient_details = []
 
         if file_type == "letter":
             file = retrieve_user_letter(file_id, user_id)
         elif file_type == "note":
             file = retrieve_note(file_id, user_id)
-            patient_details=retrieve_patient_by_email(file["patient_details"]["email"])
+            patient_details = retrieve_patient_by_email(file["patient_details"]["email"])
             del patient_details["_id"]
         elif file_type == "patient":
             file = retrieve_patient_by_id(file_id, practice_id)
