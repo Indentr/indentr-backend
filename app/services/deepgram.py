a167764b6368b8ae7d86c2b61c387f37d9de1fe6
Robert Lee <49095226+robbleee@users.noreply.github.com>
2024-05-09 14:18:39 +0100

    ruff

--- app/services/deepgram.py
+++ app/services/deepgram.py
@@ -19,9 +19,9 @@ async def dpg_speech_to_text(audio_buffer):
         options = PrerecordedOptions(
             model="nova-2",
             language="en",
-            smart_format=True, 
-            punctuate=True, 
-            paragraphs=True, 
+            smart_format=True,
+            punctuate=True,
+            paragraphs=True,
         )
 
         response = deepgram.listen.prerecorded.v("1").transcribe_file(source, options)
