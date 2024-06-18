6768f835758cec2791c9b009985ecacf628a4d43
Joe Thompson <thompsonjoejj@gmail.com>
2024-06-18 16:26:03 +0100

    Fix for search_files - makes sure dob exists in patient_details before del

--- app/routes/files.py
+++ app/routes/files.py
@@ -339,7 +339,9 @@ def search_files(body: SearchFiles, access_token=Depends(JWTBearer())):
 
             if "patient_id" in i:
                 patient_details = retrieve_patient_by_id(str(i["patient_id"]), practice_id)
-                del patient_details["dob"]
+                if "dob" in patient_details:
+                    del patient_details["dob"]
+
                 del i["patient_id"]
                 i["patient_details"] = patient_details
 
